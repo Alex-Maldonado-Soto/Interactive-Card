869c23ace0fb64d8a28c63b351cffd9c652ba872
Alex-Maldonado <alexandermaldonadosoto@gmail.com>
2023-11-27 19:36:24 -0600

    Archivos base del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button, Container } from "@mui/material";
+
+
+export default function App() {
+    return (
+      <>
+      <Container>
+        <h1>App</h1>
+        <Button variant="contained">Button</Button>
+      </Container>
+      </>
+    )
+}
